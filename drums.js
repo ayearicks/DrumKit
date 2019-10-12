c2aed717a859d9f98c9bccde102d13525cd61de6
Ashley Yearicks <ashley.yearicks@gmail.com>
2019-10-12 15:20:59 -0400

    Add touch support
    Touch support enabled on mobile devices.

--- drums.js
+++ drums.js
@@ -1,13 +1,30 @@
-// 
 const keys = Array.from(document.querySelectorAll('.key'));
+const touchKey = Array.from(document.querySelectorAll('.key'));
+const touchText = Array.from(document.querySelectorAll('.sound'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+// Listen for Touch
+touchKey.forEach(touch => touch.addEventListener('touchstart', touchSound, true));
+touchText.forEach(touchWord => touchWord.addEventListener('touchstart', touchSound, true));
+// Listen for Keyboard
+window.addEventListener('keydown', keySound);
+
+function touchSound(e) {
+    const key = document.querySelector(`.key[data-key="${e.target.getAttribute('data-key')}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.target.getAttribute('data-key')}"]`);
+    // If audio doesn't match key, stop the function
+    if (!audio) { return; }
+    // Class for adding stylized changes to buttons
+    key.classList.add('playing');
+    // Restarts sound loop, and plays
+    audio.currentTime = 0;
+    audio.play();
+}
 
 // Plays sound according to key pressed
-function playSound(e) {
+function keySound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    // When the audio stops, stop the function
+    // If audio doesn't match key, stop the function
     if (!audio) {return;}
     // Class for adding stylized changes to buttons
     key.classList.add('playing');
@@ -21,3 +38,4 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') {return;}
     this.classList.remove('playing');
 }
+
