fbe2004ebe37945b74873deadc361f48d1dda539
Ashley Yearicks <ashley.yearicks@gmail.com>
2021-08-06 16:41:52 -0400

    edited README, adjusted JS comments

--- drums.js
+++ drums.js
@@ -1,17 +1,22 @@
+// Global Variables
 const keys = Array.from(document.querySelectorAll('.key'));
 const touchKey = Array.from(document.querySelectorAll('.key'));
-// const touchText = Array.from(document.querySelectorAll('.sound'));
+
+// Assign an event and function for each key
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-// Listen for Touch
+
+// Assign an event and function for each touch
 touchKey.forEach(touch => touch.addEventListener('touchstart', touchSound, true));
-// touchText.forEach(touchWord => touchWord.addEventListener('touchstart', touchSound, true));
+
 // Listen for Keyboard
 window.addEventListener('keydown', keySound);
 
+
+// function to play mobile sound based on button touched.
 function touchSound(e) {
     const key = document.querySelector(`.key[data-key="${e.target.getAttribute('data-key')}"]`);
     const audio = document.querySelector(`audio[data-key="${e.target.getAttribute('data-key')}"]`);
-    // If audio doesn't match key, stop the function
+    // If audio doesn't match next button pressed, stop the function
     if (!audio) { return; }
     // Class for adding stylized changes to buttons
     key.classList.add('playing');
@@ -20,11 +25,11 @@ function touchSound(e) {
     audio.play();
 }
 
-// Plays sound according to key pressed
+// Plays sound according to key pressed on desktop
 function keySound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    // If audio doesn't match key, stop the function
+    // If audio doesn't match next key pressed, stop the function
     if (!audio) {return;}
     // Class for adding stylized changes to buttons
     key.classList.add('playing');
@@ -32,6 +37,7 @@ function keySound(e) {
     audio.currentTime = 0;
     audio.play();
 }
+
 
 // Function to remove class when sound is done
 function removeTransition(e) {
